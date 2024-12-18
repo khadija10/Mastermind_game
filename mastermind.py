4fdb68b3ef919c08d6abc553db5ae6a757fe9b83
khadija10 <61631159+khadija10@users.noreply.github.com>
2024-12-18 15:06:58 +0100

    Adding etape_4: counting correct positions and correct numbers

--- mastermind.py
+++ mastermind.py
@@ -9,7 +9,7 @@ MAX_ATTEMPTS = 10
 POSSIBLE_VALUES = [str(index) for index in range(1, 7)]  # liste de valuers possibles (1 à 6) [Comprehension list ]
 
 #Étape 2 : Génération de la Combinaison Secrète
-def generate_secret_combination():
+def generate_random_combination():
    
     return [random.choice(POSSIBLE_VALUES) for _ in range(COMBINATION_LENGTH)]
 
@@ -29,12 +29,30 @@ def get_player_combination():
         
         return list(combination)
     
+#Étape 4 : Vérification de la Proposition
+    
+def check_guess(random_combination, player_combination):
+
+    correct_position = sum(1 for index in range(COMBINATION_LENGTH) if player_combination[index] == random_combination[index])
+    
+    # compter les mal placés
+    random_count = {number: random_combination.count(number) for number in POSSIBLE_VALUES}
+    guess_count = {number: player_combination.count(number) for number in POSSIBLE_VALUES}
+    
+    correct_number = sum(min(random_count.get(number, 0), guess_count.get(number, 0)) for number in POSSIBLE_VALUES) - correct_position
+    
+    return correct_position, correct_number
+
 if __name__ == "__main__":
 
-    secret_combination = generate_secret_combination()
+    random_combination = generate_random_combination()
     
-    print(secret_combination)
+    print(random_combination)
 
     player_combination = get_player_combination()
 
     print(player_combination)
+
+    correct_position, correct_number = check_guess(random_combination, player_combination)
+
+    print(correct_position, correct_number)
